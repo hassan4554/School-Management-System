823ddb039b7de511543a86d6485f8c7655671feb
hassan4554 <hm9536057@gmail.com>
2025-07-05 19:43:49 +0500

    count chart bug handled

--- src/components/CountChartContainer.tsx
+++ src/components/CountChartContainer.tsx
@@ -22,12 +22,12 @@ const CountChartContainer = async () => {
                 <div className="flex flex-col gap-1">
                     <div className="w-5 h-5 bg-lamaSky rounded-full" />
                     <h1 className="font-bold">{boys}</h1>
-                    <h2 className="text-xs text-gray-300">Boys ({(boys / (boys + girls)) * 100}%)</h2>
+                    <h2 className="text-xs text-gray-300">Boys ({Math.round((boys / (boys + girls)) * 100)}%)</h2>
                 </div>
                 <div className="flex flex-col gap-1">
                     <div className="w-5 h-5 bg-lamaYellow rounded-full" />
                     <h1 className="font-bold">{girls}</h1>
-                    <h2 className="text-xs text-gray-300">Girls ({(girls / (boys + girls)) * 100}%)</h2>
+                    <h2 className="text-xs text-gray-300">Girls ({Math.round((girls / (boys + girls)) * 100)}%)</h2>
                 </div>
             </div>
         </div>
